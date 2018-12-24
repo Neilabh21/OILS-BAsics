223842b4a814a114b196bd8e0d6841058eaa870a
Neilabh Banzal <34180039+Neilabh21@users.noreply.github.com>
2018-12-24 05:59:38 +0530

    Start signal for receiving
    This was an attempt to create a kind of system where ATXmega start receiving only when it is given a start signal, idea taken from networks.
    It stops unnecessary triggering of any sensitive data / or an accidental harmful command to go through.

--- PySerial Codes/PyXTrans1 - First code/PyXTrans1/main.cpp
+++ PySerial Codes/PyXTrans1 - First code/PyXTrans1/main.cpp
@@ -1,52 +1,59 @@
-#define F_CPU 2000000                                    //Defining the clock frequency to be 2 MHz
+#define F_CPU 2000000								//Defining the clock frequency to be 2 MHz
 #include <avr/io.h>
 #include <util/delay.h>
 #include <stdio.h>
 #include <avr/interrupt.h>
 #include <math.h>
 
-volatile uint8_t d;                                          //The number to be fed and processed for calculating the value
-volatile bool count;                                      //To check if interrupt has been received
+// volatile uint8_t d;								// ***** The number to be fed and processed for calculating the value
+volatile bool count;								// To check if interrupt has been received
+// Above variables were defined while thinking we might have to use ISRs, which were ultimately not used.
 
 void transmit(uint8_t dat)
 {
-	while(!(USARTC0_STATUS & (USART_DREIF_bm)));         //Until data register empty interrupt flag is set, wait
-	USARTC0_DATA = dat;                                  //Load the data into the transmit buffer register
+	// USART transmit function.
+	while(!(USARTC0_STATUS & (USART_DREIF_bm)));				//Until data register empty interrupt flag is set, wait
+	USARTC0_DATA = dat;							//Load the data into the transmit buffer register
 }
 uint8_t receive(void)
 {
-	while(!(USARTC0_STATUS & (USART_RXCIF_bm)));         //Until receive complete interrupt flag is set, wait
-	return USARTC0_DATA;                                 //Access the receive buffer register
+	// USART recieve function.
+	while(!(USARTC0_STATUS & (USART_RXCIF_bm)));      		   	// Until receive complete interrupt flag is set, wait
+	return USARTC0_DATA;                              		   	// Access the receive buffer register
 }
 int main(void)
 {
-	PORTC_DIRSET=PIN3_bm;                                //Setting TX pin as output
-	PORTC_DIRCLR=PIN2_bm;	                             //Setting RX pin as input
-	USARTC0_BAUDCTRLA = 12;                              //Setting the BSEL number in the baud rate register for rate of 9600
-	USARTC0_BAUDCTRLB = 0; //Setting the BSCALE = 0
-	USARTC0_CTRLB = (USART_RXEN_bm)|(USART_TXEN_bm);     //Enable transmitter and receiver port
-	USARTC0_CTRLC = (USART_CHSIZE_8BIT_gc)|(USART_PMODE_DISABLED_gc);  //8 bits with 1 stop bit
-	//sei();
-    PORTF_DIRSET=0xFF;
-	count=false;
-	while(1)
+	// Main function in the program.
+	PORTC_DIRSET = PIN3_bm;							// Setting TX pin as output - C3
+	PORTC_DIRCLR = PIN2_bm;							// Setting RX pin as input - C2
+	USARTC0_BAUDCTRLA = 12;							// Setting the BSEL number in the baud rate register for rate of 9600
+	USARTC0_BAUDCTRLB = 0;							// Setting the BSCALE = 0
+	USARTC0_CTRLB = (USART_RXEN_bm)|(USART_TXEN_bm);			// Enable transmitter and receiver port
+	USARTC0_CTRLC = (USART_CHSIZE_8BIT_gc)|(USART_PMODE_DISABLED_gc);	// 8 bits with 1 stop bit
+	//sei();								// ***** Global interrupt declared for potential usage.
+    PORTF_DIRSET=0xFF;								
+	count=false;								// Boolean variable used here.
+	while(1)								// Infinite loop, will proceed further only on getting the required inute.
 	{
-        PORTF.OUT=0xFF;
+        	PORTF.OUT=0xFF;
 		d=receive();
 		if(d == 0x61)
 		{
-        PORTF.OUT=0x00;
-		while(1)
-		{
-		 uint8_t e=receive();
-		 if(e == 0xFF)
-		 {
-		  break;
-		 }
-		 transmit(e);
+        		PORTF.OUT=0x00;
+			while(1)
+			{
+				uint8_t e=receive();
+		 		if(e == 0xFF)
+		 		{
+		  			break;
+		 		}
+		 		transmit(e);
+			}
+		 	_delay_ms(1000);
 		}
-		 _delay_ms(1000);
-		}
+	}
+}
+// The following code was for debugging and is no longer useful.
      //d=receive();
      //count=true;
      //transmit('d');
@@ -63,6 +70,8 @@ int main(void)
 	}
 }
 
+
+// The following is the global interrupt and is no longer useful
 /*ISR(USARTC0_RXC_vect)
 {
  d=receive();
