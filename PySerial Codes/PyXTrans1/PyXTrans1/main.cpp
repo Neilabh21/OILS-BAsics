49d55ef4bfd4bac48db53bc51d5dff5a58e14b59
Neilabh Banzal <34180039+Neilabh21@users.noreply.github.com>
2019-01-06 16:41:06 +0530

    The changes done locally have been pushed to Git.
    Final tested code.

--- PySerial Codes/PyXTrans1/PyXTrans1/main.cpp
+++ PySerial Codes/PyXTrans1/PyXTrans1/main.cpp
@@ -1,16 +1,16 @@
-#define F_CPU 2000000						//Defining the clock frequency to be 2 MHz
-#include <avr/io.h>						// Necessary for AVR I/P / O/P
-#include <util/delay.h>						// ***** Can be removed. Necessary for delays during debugging.
-#include <stdio.h>						// Including stdio.h for ome functional requirements
-#include <avr/interrupt.h>					// ***** Can be commented out. not being used.
-#include <math.h>						// Will be used for actual calculations.
+#define F_CPU 2000000									//Defining the clock frequency to be 2 MHz
+#include <avr/io.h>										// Necessary for AVR I/P / O/P
+#include <util/delay.h>									// ***** Can be removed. Necessary for delays during debugging.
+#include <stdio.h>										// Including stdio.h for ome functional requirements
+#include <avr/interrupt.h>								// ***** Can be commented out. not being used.
+#include <math.h>										// Will be used for actual calculations.
 
 void transmit(uint8_t dat)
 {
 	// This is the transmit function using the USART functionality of ATXmega128A3U.
 	// Here, we transmit the data 'dat', which has to be 8 bits of data, or 1 byte.
 	while(!(USARTC0_STATUS & (USART_DREIF_bm)));		// Until data register empty interrupt flag is set, wait. (So that wrong data is not sent.)
-	USARTC0_DATA = dat;					// Load the data into the transmit buffer register. (Sends the data)
+	USARTC0_DATA = dat;									// Load the data into the transmit buffer register. (Sends the data)
 }
 
 uint8_t receive(void)
@@ -18,35 +18,35 @@ uint8_t receive(void)
 	// This is the receive function using the USART functionality of ATXmega128A3U.
 	// Here, we receive data in packets of 8 bits, or a byte.
 	while(!(USARTC0_STATUS & (USART_RXCIF_bm)));		// Until receive complete interrupt flag is set, wait
-	return USARTC0_DATA;					// Access the receive buffer register and return the contents of the buffer register.
+	return USARTC0_DATA;								// Access the receive buffer register and return the contents of the buffer register.
 }
 
 int main(void)
 {
 	// This is the main function of the program.
-	PORTC_DIRSET = PIN3_bm;					// Setting TX pin as output (C3)
-	PORTC_DIRCLR = PIN2_bm;					// Setting RX pin as input (C2)
-	USARTC0_BAUDCTRLA = 12;					// Setting the BSEL number in the baud rate register for rate of 9600
-	USARTC0_BAUDCTRLB = 0;					// Setting the BSCALE = 0
+	PORTC_DIRSET = PIN3_bm;								// Setting TX pin as output (C3)
+	PORTC_DIRCLR = PIN2_bm;								// Setting RX pin as input (C2)
+	USARTC0_BAUDCTRLA = 12;								// Setting the BSEL number in the baud rate register for rate of 9600
+	USARTC0_BAUDCTRLB = 0;								// Setting the BSCALE = 0
 	USARTC0_CTRLB = (USART_RXEN_bm)|(USART_TXEN_bm);	// Enable transmitter and receiver ports.
 	USARTC0_CTRLC = (USART_CHSIZE_8BIT_gc)|(USART_PMODE_DISABLED_gc);  // 8 bits with 1 stop bit.
 	
-	// pi = 3.1415926 // These are the vvarious values of 32 bit HEX numbers, which were tested using ATXmega.
+	// pi = 3.1415926 // These are the various values of 32 bit HEX numbers, which were tested using ATXmega.
 	// 0x3f9d70a4
 	// 0x12345678
 	
 	// The actual data is to be received from Python via a start flag (From Python) followed by 4 bytes of data, which will be stored in the variables a, b, c, d.
 	// That code will look something like this:
 	while(1){
-		head1 = receive();
+		uint8_t head1 = receive();
 		if (head1 == 'a'){
 			break;
 		}
 	}
-	int a = receive();					// These are the received data from the Python Console, stored in a, b, c and d.
-	int b = receive();
-	int c = receive();
-	int d = recieve();
+	uint8_t a = receive();					// These are the received data from the Python Console, stored in a, b, c and d.
+	uint8_t b = receive();
+	uint8_t c = receive();
+	uint8_t d = receive();
 	
 	/*
 	int a = 0x3f;						// These are superfluous as these have already been defined from the received data from Python.
@@ -64,7 +64,7 @@ int main(void)
 	union IntFloat val;					// This is an instance of the union created in the above line.
 	int i;							// *****
 	char buff[10];						// Declare the 1st buffer for string.
-	char buff2[10];						// Decalring the 2nd buffer for string, to be used later.
+	char buff2[10];						// Declaring the 2nd buffer for string, to be used later.
 
 	i=0;							// *****
 	
@@ -109,6 +109,7 @@ int main(void)
 	
 	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~The function in val.f comes here.~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 	// val.f = val.f * 2;
+	val.f = val.f*val.f*val.f;
 	
 	// The following function takes the 32-bit integer value from the union val (in the HEX form), sparates it into 4 packets of 8 bits each and the sends them one by one.
 	sprintf(buff2,"%lx", val.i);				// This writes the val.i into the 2nd buffer, buff2.
